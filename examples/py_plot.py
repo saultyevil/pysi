e3d701d15074a89a9f8012cbfdfbf91568a22e51
Edward <e.j.parkinson@soton.ac.uk>
2020-02-10 18:49:25 +0000

    Add __doc__ to ouline purpose of the script

--- examples/py_plot.py
+++ examples/py_plot.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+"""
+The purpose of this script is to tie together a bunch of plotting scripts into
+one script. This way, instead of running each plotting script, one only needs
+to run this script. However, the plots will be created with a default set of
+parameters to keep this script simple. If you want more flexible options,
+use (or edit) the other plotting scripts to fit your needs appropriately.
+"""
