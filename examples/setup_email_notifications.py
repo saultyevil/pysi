6b0ac57756d88b58fc893b80f582eb560452efce
Edward Parkinson <e.j.parkinson@soton.ac.uk>
2020-12-21 15:59:54 +0000

    add simple script to enable email notifications

---
+++ examples/setup_email_notifications.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from socket import gethostname
+from pyPython import mailNotifications
+
+print("Setting up email notifications by sending a message...")
+
+message = mailNotifications.send_notification(
+    "ejp1n17@soton.ac.uk", "Token creation success", 
+    "The computer {} has been setup to send email notifications".format(gethostname)
+)
+
+print(message)
+print("Success, I hope!")
