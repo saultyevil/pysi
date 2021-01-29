9aa2b886e611c94702233ab5bddfbea723f2d3e5
Edward John Parkinson <e.j.parkinson@soton.ac.uk>
2021-01-29 15:06:10 +0000

    Add imports to __init__.py

--- pypython/__init__.py
+++ pypython/__init__.py
@@ -2,3 +2,19 @@ import sys
 
 name = "pypython"
 sys.path.append("physics")
+
+import pypython.atomicdata
+import pypython.conversion
+import pypython.error
+import pypython.grid
+import pypython.lineanalysis
+import pypython.log
+import pypython.mailnotifs
+import pypython.plotutil
+import pypython.simulation
+import pypython.spectrum
+import pypython.spectrumcreate
+import pypython.spectrumplot
+import pypython.util
+import pypython.windplot
+import pypython.windutil
--- setup.py
+++ setup.py
@@ -6,17 +6,18 @@ from setuptools import setup
 setup(
       name="pypython",
       python_requires='>=3.5',
-      version="1.3",
+      version="2.0a",
       description="A Python package to make using Python a wee bit easier.",
       url="https://github.com/saultyevil/pyPython",
       author="Edward J. Parkinson",
       author_email="e.j.parkinson@soton.ac.uk",
       license="MIT",
-      packages=["pypython"],
+      packages=["pypython", "physics"],
       zip_safe=False,
       install_requires=[
-            "matplotlib", "scipy", "numpy", "pandas", "astropy", "numba", "psutil",
-            "pathlib", "argparse", "google-api-python-client", "google-auth-httplib2",
-            "google-auth-oauthlib", "sphinx", "karma_sphinx_theme"
+            "matplotlib", "scipy", "numpy", "pandas", "astropy", "numba",
+            "psutil", "pathlib", "argparse", "google-api-python-client",
+            "google-auth-httplib2", "google-auth-oauthlib", "sphinx",
+            "karma_sphinx_theme"
       ]
 )
