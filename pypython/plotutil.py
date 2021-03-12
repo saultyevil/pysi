31548219be8805dad6e52ae39d7830ab0d52920f
Edward John Parkinson <e.j.parkinson@soton.ac.uk>
2021-03-12 12:25:37 +0000

    Add function to apply JM's default matplotlib parameters

--- pypython/plotutil.py
+++ pypython/plotutil.py
@@ -12,6 +12,47 @@ from .physics.constants import C, ANGSTROM
 from .extrautil.error import DimensionError
 
 
+def set_mod_defaults():
+    """Set default pypython matplotlib parameters."""
+    
+    # Use TeX typesetting for the figure
+    
+    plt.rcParams["text.usetex"] = "True"
+    
+    # Use the following font family, sizes, etc.
+    
+    plt.rcParams["font.serif"] = ["cm"]
+    plt.rcParams["font.family"] = "serif"	
+    plt.rcParams["text.latex.preamble"] = r"\usepackage{amsmath}"
+    plt.rcParams["font.size"] = 18
+    plt.rcParams["xtick.labelsize"] = 15
+    plt.rcParams["ytick.labelsize"] = 15
+    plt.rcParams["legend.fontsize"] = 14
+    plt.rcParams["axes.titlesize"] = 16
+    plt.rcParams["axes.labelsize"] = 16
+    plt.rcParams["axes.linewidth"] = 2
+    plt.rcParams["lines.linewidth"] = 2.2
+    
+    # Use the following choices for the ticks in the panels
+    
+    plt.rcParams["xtick.top"] = "True"
+    plt.rcParams["xtick.bottom"] = "True"
+    plt.rcParams["xtick.minor.visible"] = "True"
+    plt.rcParams["xtick.direction"] = "out"
+    plt.rcParams["ytick.left"] = "True"
+    plt.rcParams["ytick.right"] = "True"
+    plt.rcParams["ytick.minor.visible"] = "True"
+    plt.rcParams["ytick.direction"] = "out"
+    plt.rcParams["xtick.major.width"] = 1.5
+    plt.rcParams["xtick.minor.width"] = 1
+    plt.rcParams["xtick.major.size"] = 4
+    plt.rcParams["xtick.minor.size"] = 3
+    plt.rcParams["ytick.major.width"] = 1.5
+    plt.rcParams["ytick.minor.width"] = 1
+    plt.rcParams["ytick.major.size"] = 4
+    plt.rcParams["ytick.minor.size"] = 3
+
+
 def subplot_dims(
     n_plots: int
 ) -> Tuple[int, int]:
