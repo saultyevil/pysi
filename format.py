16a56d5cef7dae818342e836ed5d3dea9db082c9
Edward Parkinson <e.j.parkinson@soton.ac.uk>
2021-05-11 22:52:55 +0300

    Update format.py

--- format.py
+++ format.py
@@ -1,17 +1,38 @@
+#!/usr/bin/env python3
+
+from sys import argv
 from subprocess import run
 from pathlib import Path
 
-for path in Path("pypython").rglob("*.py"):
-    print(path)
-    cmd = f"yapf -i {path} > /dev/null"
-    run(cmd, shell=True)
-    cmd = f"isort {path} > /dev/null"
-    run(cmd, shell=True)
 
-for path in Path("scripts").rglob("*.py"):
-    print(path)
-    cmd = f"yapf -i {path} > /dev/null"
-    run(cmd, shell=True)
-    cmd = f"isort {path} > /dev/null"
-    run(cmd, shell=True)
+def format_source(fp):
+    """Run isort and then yapf to format the python files contained in
+    fp. Sends the output to /dev/null.
+    Parameters
+    ----------
+    fp: str
+        The file path to search recursively for python files.
+    """
+    for file in Path(fp).rglob("*.py"):
+        print(str(file))
+        run(f"isort {file} > /dev/null; yapf -i {file} > /dev/null", shell=True)
 
+
+def strip_type_hints(fp):
+    """Stip type hints from source files.
+    Parameters
+    ----------
+    fp: str
+        The file path to search recursively for python files.
+    """
+    for file in Path(fp).rglob("*.py"):
+        print(str(file))
+        run(f"strip-hints {file}")
+
+
+if "--strip_hints" in argv:
+    strip_type_hints("pypython")
+    strip_type_hints("scripts")
+
+format_source("pypython")
+format_source("scripts")
