109f650505388ecf0611c6e2661d1365bca4cd70
Edward Parkinson <e.j.parkinson@soton.ac.uk>
2021-09-19 21:38:34 +0100

    Update version number due to new script names

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ scripts = [
 setup(
       name="pypython",
       python_requires='>=3.7',
-      version="3.5.1",
+      version="3.6.0",
       description="A package to make using Python a wee bit easier.",
       url="https://github.com/saultyevil/pypython",
       author="Edward J. Parkinson",
