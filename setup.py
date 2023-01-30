7778e76c62cc5c30148c765650d6171423b4cbfe
Edward J. Parkinson <saultyevil@gmail.com>
2023-01-30 19:38:49 +0000

    change wrong variable

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup
 
-# get requirements
+# get requirements from file
 with open("requirements.txt", "r", encoding="utf-8") as file_in:
     requirements = [line.strip("\n") for line in file_in.readlines()]
 # get version from pypython/__init__.py
@@ -11,11 +11,11 @@ with open("pypython/__init__.py", "r", encoding="utf-8") as file_in:
     lines = file_in.readlines()
 for line in lines:
     line = line.split()
-    if len(lines) < 1:
+    if len(line) < 1:
         continue
     if line[0] == "__version__":
         __version__ = str(line[2]).strip('"').strip("'")
-
+# setup function
 setup(
     name="pypython",
     python_requires="~=3.10",
