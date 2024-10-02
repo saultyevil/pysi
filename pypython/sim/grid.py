94e88055bc0a7397da4c6cd70afdd773e2042f5a
Edward J. Parkinson <saultyevil@gmail.com>
2024-10-02 18:56:17 +0100

    clean-up grid sub-module

--- pypython/sim/grid.py
+++ pypython/sim/grid.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""Create or modify a grid of parameter files."""
 
+"""Modify parameter files and construct grids of parameter files."""
+
+from __future__ import annotations
+
+from pathlib import Path
 from shutil import copyfile
 
 
-def add_parameter(fp, name, value, insert=None, backup=True, verbose=False):
+def add_parameter(
+    filepath: str,
+    parameter_name: str,
+    parameter_value: str | float,
+    *,
+    insert_after: str | None = None,
+    backup_original: bool | None = True,
+) -> None:
     """Add a parameter which doesn't already exist.
 
     The parameter will either be appended to the end of the parameter file,
@@ -13,118 +23,46 @@ def add_parameter(fp, name, value, insert=None, backup=True, verbose=False):
 
     Parameters
     ----------
-    fp: str
+    filepath : str
         The path to the parameter file
-    name: str
+    parameter_name : str
         The name of the parameter to be added
-    value: str
+    parameter_value : str
         The value of the parameter
-    insert: str [optional]
+    insert_after : str [optional]
         Insert the new parameter after this parameter
-    backup: bool [optional]
+    backup_original : bool [optional]
         Create a back up of the original parameter file
-    verbose: bool [optional]
-        Enable verbose output.
+
     """
-
-    if fp.find(".pf") == -1:
-        raise IOError(f"provided file path {fp} is not a .pf parameter file")
-
-    if backup:
-        copyfile(fp, fp + ".bak")
-
-    with open(fp, "r") as f:
+    if filepath.find(".pf") == -1:
+        raise OSError(f"provided file path {filepath} is not a .pf parameter file")
+    if backup_original:
+        copyfile(filepath, filepath + ".bak")
+    with Path.open(filepath, encoding="utf-8") as f:
         lines = f.readlines()
 
     # Get the parameters and values into a list. Removes blank lines and
     # comment lines
-
-    lines = [line.split() for line in lines if line.split() and line.startswith("###") == False]
+    lines = [line.split() for line in lines if line.split() and not line.startswith("###")]
     names = [line[0] for line in lines]
     values = [line[1] for line in lines]
 
     # Check if the parameter is already in there and use update instead.
     # Otherwise, insert the new parameter somewhere and write the new
     # parameter file out
-
-    if name in names:
-        update_parameter(fp, name, value, backup)
+    if parameter_name in names:
+        update_parameter_value(filepath, parameter_name, parameter_value, backup_original)
     else:
-        if insert:
-            where = names.index(insert) + 1
-        else:
-            where = len(names)
-        names.insert(where, name)
-        values.insert(where, value)
-
-        # Print update if verbose and write to file
-
-        if verbose:
-            header = f"- {fp} "
-            while len(header) < 80:
-                header += "-"
-            print(header)
-            print(f"   -> {name} {value}")
-
-        with open(fp, "w") as f:
-            for name, value in zip(names, values):
+        where = names.index(insert_after) + 1 if insert_after else len(names)
+        names.insert(where, parameter_name)
+        values.insert(where, parameter_value)
+        with Path.open(filepath, "w", encoding="utf-8") as f:
+            for name, value in zip(names, values, strict=True):
                 f.write(f"{name:40s} {value}\n")
 
-    return
 
-
-def create_grid(fp, name, values, extra_name=None, backup=True, verbose=False):
-    """Create a bunch of new parameter files with the choice of values for a
-    given parameter.
-
-    This will only work for one parameter at a time and one parameter file.
-    By default, a back up of the original parameter file is made as a safety
-    precaution.
-
-    Parameters
-    ----------
-    fp: str
-        The path to the base parameter file to construct the grid from
-    name: str
-        The name of the parameter to create a grid of
-    values: List[str]
-        A list of values for the simulation grid for the parameter
-    extra_name: str [optional]
-        Adds an util name to the output grid parameter file names
-    backup: bool [optional]
-        Create a back up of the original parameter file
-    verbose: bool [optional]
-        Enable verbose output to the screen
-
-    Returns
-    -------
-    grid: List[str]
-        The paths to the newly generated parameter files for the grid
-    """
-
-    grid = []
-    n_grid = len(values)
-
-    if backup:
-        copyfile(fp, fp + ".bak")
-
-    ext = fp.find(".pf")
-    if ext == -1:
-        raise IOError(f"provided file path {fp} is not a .pf parameter file")
-
-    for i in range(n_grid):
-        fp_new = fp[:ext]
-        if extra_name:
-            fp_new += "_{}".format(extra_name)
-        fp_new += "_{}".format(values[i]) + ".pf"
-        copyfile(fp, fp_new)
-        update_parameter(fp_new, name, values[i], backup=False, verbose=verbose)
-        grid.append(fp_new)
-
-    return grid
-
-
-def get_parameter(fp, name):
+def get_parameter_value(filepath: str, parameter_name: str) -> str:
     """Get the value for a parameter in a parameter file.
 
     The entire parameter file is searched to find the given parameter and
@@ -133,39 +71,38 @@ def get_parameter(fp, name):
 
     Parameters
     ----------
-    fp: str
+    filepath : str
         The path to the parameter file
-    name: str
+    parameter_name : str
         The name of the parameter
 
     Returns
     -------
-    value: str
-        The value of the parameter
+    value : str
+        The value of the parameter, as a string.
+
     """
-
-    if fp.find(".pf") == -1:
-        raise IOError(f"Provided file path {fp} is not to a parameter file")
-
-    with open(fp, "r", encoding="utf-8") as file_in:
+    if filepath.find(".pf") == -1:
+        raise OSError(f"Provided file path {filepath} is not to a parameter file")
+    with Path.open(filepath, encoding="utf-8") as file_in:
         lines = file_in.readlines()
 
     value = None
-
     for line in lines:
-        if line.find(name) != -1:
+        if line.find(parameter_name) != -1:
             split = line.split()
-            if len(split) != 2:
-                raise IndexError(f"Invalid syntax for {name} in {fp}")
+            if len(split) != 2:  # noqa: PLR2004
+                raise IndexError(f"Invalid syntax for {parameter_name} in {filepath}")
             return split[-1]
-
     if value is None:
-        raise ValueError(f"Could not find the parameter {name} in {fp}")
+        raise ValueError(f"Could not find the parameter {parameter_name} in {filepath}")
 
     return value
 
 
-def update_parameter(fp, name, new_value, backup=True, verbose=False):
+def update_parameter_value(
+    filepath: str, parameter_name: str, parameter_value: str | float, *, backup_original: bool = True
+) -> None:
     """Change the value of a parameter in a Python parameter file.
 
     If the old and new parameter value are the same, the script will still
@@ -173,48 +110,90 @@ def update_parameter(fp, name, new_value, backup=True, verbose=False):
 
     Parameters
     ----------
-    fp: str
+    filepath : str
         The path to the parameter file
-    name: str
+    parameter_name: str
         The name of the parameter to update
-    new_value: str
+    parameter_value : str
         The updated value of the parameter
-    backup: bool [optional]
+    backup_original : bool [optional]
         Create a back up of the original parameter file
-    verbose: bool [optional]
-        Enable verbose output to the screen
+
     """
-
-    if fp.find(".pf") == -1:
-        raise IOError(f"The provided file path {fp} is not to a parameter file")
-
-    if backup:
-        copyfile(fp, fp + ".bak")
+    if filepath.find(".pf") == -1:
+        raise OSError(f"The provided file path {filepath} is not to a parameter file")
+    if backup_original:
+        copyfile(filepath, filepath + ".bak")
+    with Path.open(filepath, encoding="utf-8") as file_in:
+        lines = file_in.readlines()
 
     old = ""
     new = ""
-
-    with open(fp, "r", encoding="utf-8") as file_in:
-        lines = file_in.readlines()
-
     for i, line in enumerate(lines):
-        if line.find(name) != -1:
+        if line.find(parameter_name) != -1:
             old = line
-            new = f"{name:40s} {new_value}\n"
+            new = f"{parameter_name:40s} {parameter_value}\n"
             lines[i] = new
             break
+    if not old and not new:
+        raise ValueError(f"Could not find the parameter {parameter_name} in {filepath}")
 
-    if old and new:
-        if verbose:
-            header = f"- {fp} "
-            while len(header) < 80:
-                header += "-"
-            print(header)
-            print(f"   {name}: {old.split()[-1]} -> {new_value}")
-    else:
-        raise ValueError(f"Could not find the parameter {name} in {fp}")
-
-    with open(fp, "w", encoding="utf-8") as file_out:
+    with Path.open(filepath, mode="w", encoding="utf-8") as file_out:
         file_out.writelines(lines)
 
-    return
+
+def create_grid(
+    filepath: str,
+    parameter_name: str,
+    parameter_values: list[str | float],
+    *,
+    grid_name: str | None = None,
+    backup_original: bool = True,
+) -> None:
+    """Create a grid of parameter files for a given parameter.
+
+    This creates a grid of parameter files for the given garameter for the
+    provided values. Note that this will work for only a single parameter at a
+    time and will use an existing parameter file as input for the rest of the
+    parameters.
+
+    By default, a backup of the original parameter file is made.
+
+    Parameters
+    ----------
+    filepath : str
+        The path to the base parameter file to construct the grid from
+    parameter_name : str
+        The name of the parameter to create a grid of
+    parameter_values : List[str | float]
+        A list of values for the simulation grid for the parameter
+    grid_name : str [optional]
+        Adds an extra name to the output grid parameter file names, to associate
+        the parameter file to the grid.
+    backup_original : bool [optional]
+        Create a back up of the original parameter file
+
+    Returns
+    -------
+    grid_parameter_files : list[str]
+        The paths to the newly generated parameter files for the grid
+
+    """
+    grid_parameter_files = []
+    n_grid = len(parameter_values)
+    if backup_original:
+        copyfile(filepath, filepath + ".bak")
+    ext = filepath.find(".pf")
+    if ext == -1:
+        raise OSError(f"provided file path {filepath} is not a .pf parameter file")
+
+    for i in range(n_grid):
+        fp_new = filepath[:ext]
+        if grid_name:
+            fp_new += f"_{grid_name}"
+        fp_new += f"_{parameter_values[i]}" + ".pf"
+        copyfile(filepath, fp_new)
+        update_parameter_value(fp_new, parameter_name, parameter_values[i], backup_original=False)
+        grid_parameter_files.append(fp_new)
+
+    return grid_parameter_files
