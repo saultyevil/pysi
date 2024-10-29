7af7349d591e6d69e573050afdd831d9963610be
Edward J. Parkinson <saultyevil@gmail.com>
2024-10-29 20:27:15 +0000

    moving conversion functions into own sub-module

--- pysi/math/__init__.py
+++ pysi/math/__init__.py
@@ -6,39 +6,3 @@ quantities governed my various laws and equations, i.e. gravitational
 radius. It also includes functions for converting quantities into other
 quantities.
 """
-
-from astropy.constants import c  # pylint: disable=no-name-in-module
-
-from pysi.math.constants import ANGSTROM
-
-
-def angstrom_to_hz(wavelength):
-    """Convert a wavelength from Angstroms into a frequency.
-
-    Parameters
-    ----------
-    wavelength: float
-        The wavelength in Angstroms.
-
-    Returns
-    -------
-    The frequency in Hertz.
-
-    """
-    return c.cgs / (wavelength * ANGSTROM)
-
-
-def hz_to_angstrom(frequency):
-    """Convert a frequency in Hz to a wavelength in Angstroms.
-
-    Parameters
-    ----------
-    frequency: float
-        The frequency in Hz.
-
-    Returns
-    -------
-    The wavelength in Angstroms.
-
-    """
-    return c.cgs / frequency / ANGSTROM
