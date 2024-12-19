f24aa36683cb56a2edab4738fd1a78eb40084af7
Edward J. Parkinson <saultyevil@gmail.com>
2024-12-19 20:03:59 +0000

    clean up and remove unused code

--- pysi/math/blackhole.py
+++ pysi/math/blackhole.py
@@ -4,27 +4,10 @@ All functions require the mass to be in units of solar masses and
 accretion rates in solar masses per year.
 """
 
-from astropy.constants import G, M_sun, R_sun, c
+from astropy.constants import G, M_sun, c
 
 
-def innermost_stable_circular_orbit(m_bh):
-    """Calculate the radius of the innermost stable circular orbit of a black
-    hole of given mass Mbh.
-
-    Parameters
-    ----------
-    m_bh: float
-        The mass of the black hole in solar masses.
-
-    Returns
-    -------
-    The radius of the innermost stable circular orbit in cm.
-
-    """
-    return 3 * schwarzschild_radius(m_bh)
-
-
-def gravitational_radius(m_bh):
+def gravitational_radius(m_bh: float) -> float:
     """Calculate the gravitational radius of a black hole of given mass Mbh.
 
     Parameters
@@ -40,7 +23,7 @@ def gravitational_radius(m_bh):
     return G.cgs.value * m_bh * M_sun.cgs.value / c.cgs.value**2
 
 
-def schwarzschild_radius(m_bh):
+def schwarzschild_radius(m_bh: float) -> float:
     """Calculate the Schwarzschild radius of a black hole of given mass Mbh.
 
     Parameters
@@ -54,19 +37,3 @@ def schwarzschild_radius(m_bh):
 
     """
     return 2 * gravitational_radius(m_bh)
-
-
-def tidal_disruption_radius(m_bh, m_star, r_star):
-    """Calculate the disruption radius of a black hole for a given solar mass.
-
-    Parameters
-    ----------
-    m_bh: float
-        The mass of the black hole in solar masses.
-    m_star: float
-        The mass of the disrupted star in solar masses.
-    r_star: float
-        The radius of the disrupted star in solar radii.
-
-    """
-    return r_star * (m_bh / m_star) ** (1 / 3) * R_sun.cgs.value
